d06feb9a827482f12d08013046351cba936b8dfd
Lordiii <lordiii@lordiii.de>
2024-03-19 17:34:09 +0100

    Implement Open Graph Protocol

--- ifs.py
+++ ifs.py
@@ -15,24 +15,26 @@ ifs_images = ifs_data["images"]
 ifs_count = len(ifs_images)
 total = 0
 for image in ifs_images:
-    image_id = int(image["filename"].split(".")[0])
+    image_id = int(image["filename"].split(".")[0]) + 1
     image_id_str = str(image_id)
 
     frontmatter = '+++\n'
-    frontmatter += 'title = "IFS ' + image_id_str + '"\n'
+    frontmatter += 'title = "Image From Space ' + image_id_str + '"\n'
     frontmatter += 'weight = ' + str(ifs_count - image_id) + '\n'
+    frontmatter += 'template = "ifs/ifs-single.html"\n'
     frontmatter += '[extra]\n'
     frontmatter += 'image_filename = "' + image["filename"] + '"'
     frontmatter += "\nwidth = " + str(image["width"])
     frontmatter += "\nheight = " + str(image["height"])
+    frontmatter += "\nimage_id = " + image_id_str
     frontmatter += '\n+++\n'
 
     content = "\n"
     content += "\n#### Image From Space " + image_id_str + ' {{ button(text="%replace_with_text%", location="/images/ifs/all/") }}'
     content += "\n"
-    content += '{{ button(text="<<", location="/images/ifs/all/' + str(image_id + 1) + '", disabled="' + str(image_id + 1 >= (ifs_count-1)) + '") }}'
+    content += '{{ button(text="<<", location="/images/ifs/all/' + str(image_id + 1) + '", disabled="' + str(image_id + 1 >= (ifs_count)) + '") }}'
     content += '{{ ifs_image(filename="' + image["filename"] + '", height=' + str(image["height"]) + ', width=' + str(image["width"]) + ') }}'
-    content += '{{ button(text=">>", location="/images/ifs/all/' + str(image_id - 1) + '", disabled="' + str(image_id - 1 < 0) + '") }}'
+    content += '{{ button(text=">>", location="/images/ifs/all/' + str(image_id - 1) + '", disabled="' + str(image_id - 1 <= 0) + '") }}'
 
     f = open(ifs_folder_all + "/" + image_id_str + ".md", "w")
     f.write(frontmatter)
