c345934c2479abf07613f58fd0359c641cb43bd6
Lordiii <lordiii@lordiii.de>
2025-06-01 20:31:27 +0200

    Update status

--- static/js/site.js
+++ static/js/site.js
@@ -30,7 +30,7 @@ switch (location.hostname) {
    status handling
 */
 
-const ApiUrl = "https://status.kreativitaet-trifft-technik.de/api/openState";
+const StatusApiUrl = "https://status.kreativitaet-trifft-technik.de/api/statusStream?spaceOpen=1&radstelleOpen=1&machining=1&woodworking=1&lab3dOpen=1";
 
 const StateClosed = {
     name: "closed",
@@ -52,96 +52,124 @@ const StateUnknown = {
     color: "btn-info"
 };
 
-const States = {
-    text_id_prefix: "status-text-",
-    button_id_prefix: "status-",
-    all_states: [
-        StateClosed,
-        StateClosing,
-        StateOpen,
-        StateUnknown
-    ],
-    state_map: {
-        "none": StateClosed,
-        "closing": StateClosing,
-        "open": StateOpen,
-        "open+": StateOpen,
-        "unknown": StateUnknown
-    },
-};
+function set_status(room, state) {
+    console.info("Updating room state", room, state);
 
-/**
- * @param {string} id
- * @param {string} color
- */
-function set_button_color(id, color) {
-    for (let state of States.all_states) {
-        document.getElementById(id).classList.remove(state.color);
+    let element = document.getElementById("status-" + room);
+
+    if (!element) {
+        let newElement = document.getElementById("status-template").cloneNode(true);
+
+        newElement.id = newElement.id.replace("template", room);
+        newElement.classList.remove("d-none");
+
+        for (let child of newElement.children) {
+            child.id = child.id.replace("template", room);
+            child.classList.add("d-none");
+        }
+
+        let parent = document.getElementById("space-status");
+        parent.appendChild(newElement);
+
+        element = newElement;
     }
-    document.getElementById(id).classList.add(color);
-}
 
-/**
- * @param {string} room_name
- * @param {string} state_name
- * @param {string} id
- */
-function set_room_status(room_name, state_name, id) {
-    let state = States.state_map[state_name];
-    if (state) {
-        let room = Rooms.find(r => r.name === room_name);
-
-        if (room) {
-            for (let status of States.all_states) {
-                document.getElementById(`${States.text_id_prefix + status.name}-${room.name}`).classList.add("d-none");
+    for (let child of element.children) {
+        if (child.id.startsWith("status-name")) {
+            for (let room_translation of ROOMS) {
+                if (room_translation.name === room) {
+                    child.innerText = location.pathname.startsWith("/en") ? room_translation.title.en : room_translation.title.de;
+                    break;
+                } else {
+                    child.innerText = room + ":";
+                }
             }
-            document.getElementById(`${States.text_id_prefix + state.name}-${room.name}`).classList.remove("d-none");
-            set_button_color(id, state.color);
 
-            return true;
+            child.classList.remove("d-none");
         } else {
-            throw new Error(`Unknown room supplied: ${room_name}`);
-        }
-    } else {
-        set_room_status(room_name, "unknown", id);
-        throw new Error(`Unknown status option supplied: ${state_name}`);
-    }
-}
-
-async function fetch_current_status() {
-    let currentStatus = await (await fetch(ApiUrl)).json();
-    console.info("Processing current status information", currentStatus);
-
-    let status_buttons = [];
-    let status_button_elements = document.getElementById("space-status").children;
-    for (let room of status_button_elements) {
-        status_buttons.push(room.getAttribute("id"));
-    }
-
-    for (let room_name in currentStatus) {
-        try {
-            let id = States.button_id_prefix + room_name;
-            set_room_status(room_name, currentStatus[room_name].state, id);
-            status_buttons.splice(status_buttons.indexOf(id), 1);
-        } catch (error) {
-            console.error("An error occurred:", error);
+            child.classList.add("d-none");
         }
     }
 
-    for (let room_name of status_buttons) {
-        set_button_color(room_name, StateUnknown.color);
+    let public_state;
+    let color;
+
+    switch (state) {
+        case 'none':
+            public_state = 'closed';
+            color = 'danger';
+            break;
+        case 'open':
+            public_state = 'open';
+            color = 'success';
+            break;
+        case 'open+':
+            public_state = 'open';
+            color = 'success';
+            break;
+        case 'keyholder':
+            public_state = 'keyholder';
+            color = 'warning';
+            break;
+        case 'member':
+            public_state = 'member';
+            color = 'warning';
+            break;
+        case 'closing':
+            public_state = 'closing';
+            color = 'warning';
+            break;
+        default:
+            console.warn("Unknown state for room", state, room);
+            public_state = 'unknown';
+            color = 'info';
+            break;
+    }
+
+    document.getElementById("status-text-" + public_state + "-" + room).classList.remove("d-none");
+
+    element.classList.remove("btn-danger", "btn-success", "btn-warning", "btn-info");
+    element.classList.add("btn-" + color);
+}
+
+function on_status_change(event) {
+    let room = event.type;
+
+    try {
+        console.log(event.data);
+        set_status(room, JSON.parse(event.data).state);
+    } catch (err) {
+        console.error("Error while setting room status", room, err, event);
+        set_status(room, "none");
     }
 }
 
-function set_all_rooms(state_name = "closed") {
-    if (!States.state_map[status]) {
-        throw new Error(`Unknown status option supplied: ${state_name}`);
-    }
+function init_status() {
+    let status_source = new EventSource(StatusApiUrl);
+    let last_keepalive;
+    let startup_timer;
 
-    for (let room of Rooms) {
-        set_room_status(room.name, state_name, id);
-    }
+    status_source.onopen = function () {
+        console.log('EventSource is open');
+        last_keepalive = Date.now();
+    };
+
+    status_source.onerror = function (err) {
+        console.error('EventSource error.', err);
+        clearTimeout(startup_timer);
+        status_source.close();
+        setTimeout(init_status, 5000);
+    };
+
+    status_source.addEventListener('spaceOpen', on_status_change);
+    status_source.addEventListener('lab3dOpen', on_status_change);
+    status_source.addEventListener('radstelleOpen', on_status_change);
+    status_source.addEventListener('machining', on_status_change);
+    status_source.addEventListener('woodworking', on_status_change);
+
+    status_source.addEventListener('keepalive', function (e) {
+        last_keepalive = Date.now();
+    }, false);
 }
 
-fetch_current_status().then();
-setInterval(fetch_current_status, 60000);
+init_status();
