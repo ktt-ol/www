6f0c9fdd1f076ca548096e36d9f06e6210d4b024
Lordiii <lordiii@lordiii.de>
2025-06-22 18:26:28 +0200

    fix(css): Determine theme in head

--- static/js/site.js
+++ static/js/site.js
@@ -2,18 +2,6 @@
     mainframe / ktt-ol theme switch
  */
 
-function isDarkModeEnabled() {
-    return window.matchMedia && window.matchMedia('(prefers-color-scheme: dark)').matches;
-}
-
-if (isDarkModeEnabled()) {
-    document.getElementsByTagName("html")[0].dataset["bsTheme"] = "dark";
-    document.getElementById("img-icon-lang").src = "/media/img/lang/icon.dark.svg";
-} else {
-    document.getElementsByTagName("html")[0].dataset["bsTheme"] = "light";
-    document.getElementById("img-icon-lang").src = "/media/img/lang/icon.light.svg";
-}
-
 switch (location.hostname) {
     case "mainframe.io":
         document.getElementById("img-logo-mainframe").style.display = "inline";
